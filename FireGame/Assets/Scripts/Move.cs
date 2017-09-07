62f1767dec148086ca4576619b85a67d4052e0c6
LD <981824007@qq.com>
2017-09-07 18:22:03 +0800

    fistly creat pro

---
+++ FireGame/Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("firstly init");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
