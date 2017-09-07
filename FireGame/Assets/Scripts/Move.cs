467296cdcae06eddea5cc1aa9ff90192202a3bf3
LD <981824007@qq.com>
2017-09-07 18:47:15 +0800

    luo add print in awake

--- FireGame/Assets/Scripts/Move.cs
+++ FireGame/Assets/Scripts/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("firstly init");
+		print("luo changed it");
 	}
 	
 	// Update is called once per frame
